db530986a02cc7d4dcb552f228447d5db8e58be2
Alvin <Alvin@everywhere.com>
2022-05-11 19:24:37 +0800

    test

---
+++ 0010-regular-expression-matching/main.go
@@ -0,0 +1,5 @@
+package re
+
+func isMatch(s string, p string) bool {
+    return true
+}
---
+++ 0010-regular-expression-matching/main_test.go
@@ -0,0 +1,27 @@
+package re
+
+import "testing"
+
+func Test_isMatch(t *testing.T) {
+	type args struct {
+		s string
+		p string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{"a", args{"aa", "a"}, false},
+		{"b", args{"aa", "a*"}, true},
+		{"c", args{"ab", ".*"}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isMatch(tt.args.s, tt.args.p); got != tt.want {
+				t.Errorf("isMatch() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
